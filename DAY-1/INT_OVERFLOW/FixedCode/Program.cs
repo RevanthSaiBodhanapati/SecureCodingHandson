f57191c9ac959a10b941818044844ea253d42d14
RevanthSaiBodhanapati <36080760+RevanthSaiBodhanapati@users.noreply.github.com>
2025-02-12 14:57:50 +0530

    code
    added to

---
+++ DAY-1/INT_OVERFLOW/FixedCode/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SecureCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter two numbers : ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                if (!(b > 0 && a > int.MaxValue - b ||
+                b < 0 && a < int.MinValue - b))
+                {
+                    Console.WriteLine($"Result :{a+b}");
+                }
+               
+            }
+            catch(Exception theEx)
+            {
+                Console.WriteLine(theEx.Message);
+                Console.WriteLine(theEx);
+            }
+        }
+      
+    }
+}
